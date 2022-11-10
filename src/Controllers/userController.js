0d3bdc64e561fe87f897e0277d2a7b0f0a79825e
unknown <amarjeet@baruzotech.com>
2022-11-10 18:21:54 +0530

    give selected dat in response body usnig creatin a new object

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -18,7 +18,7 @@ const loginUser = async (req, res) => {
     try {
         let data = req.body  
    let user = await userModel.findOne({email:data.email}).select({_id:1})
-   const token =  jwt.sign({_id:user }, "secret", { expiresIn: '1h' });
+   const token =  jwt.sign({_id:user }, "secret", { expiresIn: '12h' });
    data.token = token
         res.status(200).send({ status: true, message: "user login succesfully", data: data })
     }
@@ -32,22 +32,30 @@ const loginUser = async (req, res) => {
 const getUserData = async (req, res) => {
     try {
     
-    let bearerToken = req.headers.authorization
+    let bearerToken = req.headers['authorization']
       if(bearerToken !== undefined){
         const bearer = bearerToken.split(' ')
         const token = bearer[1]
         let decoded = jwt.verify(token, "secret")
-        let findUser = await userModel.findOne({_id:decoded._id})
-         return res.status(200).send({ status: true, message: "user data get succesfully", data:findUser })
+        let findUser = await userModel.findOne({_id:decoded._id._id})
+        let filterData = {}
+       filterData.name = findUser.name
+       filterData.country = findUser.country
+       filterData._id = findUser._id
+       
+        console.log(filterData)
+         return res.status(200).send({ status: true, message: "user data get succesfully", data:filterData})
+      }
+      else{
+        res.status(403).send({message:"please provide valid token"})
       }
      
     }
-
     catch (error) {
         res.status(500).send({ status: false, message: error.message })
-
     }
 }
+
 
 module.exports.registerUser = registerUser
 module.exports.loginUser = loginUser
